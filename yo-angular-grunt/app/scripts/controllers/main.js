ef051c2ed1ae1f9c639263a429b99399e9742947
Landerqi <landerqi@gmail.com>
2015-07-15 17:36:22 +0800

    yo-angular-test

---
+++ yo-angular-grunt/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name yoAngularGruntApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the yoAngularGruntApp
+ */
+angular.module('yoAngularGruntApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
