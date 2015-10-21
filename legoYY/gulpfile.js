24e0eb8168b5ddfae64828dd928ca254498f5605
奇 谢 <landerqi@gmail.com>
2015-10-21 19:54:43 +0800

    删除legoYY测试代码

--- legoYY/gulpfile.js
+++
@@ -1,7 +0,0 @@
-var gulp = require('gulp'),
-	plugins = require('gulp-load-plugins')();
-
-require('./tasks/gulpfile-dev')(gulp, plugins)
-require('./tasks/gulpfile-build')(gulp, plugins)
-require('./tasks/gulpfile-publish')(gulp, plugins)
-require('./tasks/gulpfile-zip')(gulp, plugins)
